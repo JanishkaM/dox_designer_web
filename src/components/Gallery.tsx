921a45b30927284e0ab4332c3220bf2a5631e992
Janishka Madushan <janishkawebdesign@gmail.com>
2024-05-12 10:02:34 +0530

    small fix

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -20,12 +20,14 @@ export default function Gallery({ data }: GalleryProps) {
       <div className="bg-green-400 py-8">
         <div className="container columns-1 sm:columns-2 md:columns-3">
           {data.map((item) => (
-            <div className="group relative flex justify-center items-center rounded-sm">
+            <div
+              key={item.id}
+              className="group relative flex justify-center items-center rounded-sm"
+            >
               <img
                 className="py-2 rounded-sm cursor-pointer group-hover:brightness-[10%] duration-300"
                 src={item.image}
                 alt={item.name}
-                key={item.id}
               />
               <div className="hidden cursor-pointer absolute group-hover:block text-center p-4">
                 <h5 className="cursor-pointer z-10 font-extrabold text-3xl text-slate-100">
@@ -34,7 +36,7 @@ export default function Gallery({ data }: GalleryProps) {
                 <p className="cursor-pointer z-10 text-lg mb-6 text-slate-100">
                   Click below button and send your CV ID to us.
                 </p>
-                <WhatsAppButton message={item.description}/>                                
+                <WhatsAppButton message={item.description} />
               </div>
             </div>
           ))}
