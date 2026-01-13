1e76c0b409b7c1b5cee3c6a14c7ca5d265a90849
JanishkaM <janishkawebdesign@gmail.com>
2026-01-13 21:06:14 +0530

    feat: implement contact form with state management and error handling

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -13,8 +13,44 @@ import { Label } from "@/components/ui/label";
 
 import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [body, setBody] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+    setSuccess(null);
+
+    try {
+      const response = await fetch("https://docxdeveloper.com/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, subject, body }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data?.message || "Error sending email.");
+      }
+
+      setSuccess(data?.message || "Email sent successfully!");
+      setName("");
+      setEmail("");
+      setBody("");
+    } catch (err: any) {
+      console.error("Error sending email:", err);
+      setError(err.message || "Error sending email. Please try again later.");
+    }
+  };
   return (
     <main>
       <section className="py-4">
@@ -34,30 +70,55 @@ export default function Contact() {
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
-                  <form>
+                  <form method="post" onSubmit={handleSubmit}>
                     <div className="grid w-full items-center gap-4">
                       <div className="flex flex-col space-y-1.5">
                         <Label htmlFor="name">Name</Label>
-                        <Input id="name" />
+                        <Input
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                          id="name"
+                        />
                       </div>
                       <div className="flex flex-col space-y-1.5">
                         <Label htmlFor="email">Email Address</Label>
-                        <Input id="email" />
+                        <Input
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          id="email"
+                        />
                       </div>
                       <div className="flex flex-col space-y-1.5">
                         <Label htmlFor="subject">Subject</Label>
-                        <Input id="subject" />
+                        <Input
+                          value={subject}
+                          onChange={(e) => setSubject(e.target.value)}
+                          id="subject"
+                        />
                       </div>
                       <div className="flex flex-col space-y-1.5">
                         <Label htmlFor="message">Message</Label>
-                        <Textarea id="message" />
+                        <Textarea
+                          value={body}
+                          onChange={(e) => setBody(e.target.value)}
+                          id="message"
+                        />
                       </div>
                     </div>
+                    <Button type="submit">Send Email</Button>
+                    {error && (
+                      <p className="mt-4 text-sm bg-red-400 rounded-md p-4 text-white">
+                        {error}
+                      </p>
+                    )}
+                    {success && (
+                      <p className="mt-4 text-sm bg-green-400 rounded-md p-4 text-white">
+                        {success}
+                      </p>
+                    )}
                   </form>
                 </CardContent>
-                <CardFooter className="flex justify-between">
-                  <Button>Send Email</Button>
-                </CardFooter>
+                <CardFooter className="flex justify-between"></CardFooter>
               </Card>
             </motion.div>
           </div>
@@ -95,10 +156,7 @@ export default function Contact() {
               <Separator className="my-4" />
               <div className="space-y-1">
                 <h4 className="text-lg hover:text-slate-700 font-medium leading-none">
-                  <a
-                    href="mailto:mail@docxdeveloper.com"
-                    target="_blank"
-                  >
+                  <a href="mailto:mail@docxdeveloper.com" target="_blank">
                     <i className="bi bi-envelope pe-2 text-3xl"></i>{" "}
                     mail@docxdeveloper.com
                   </a>
