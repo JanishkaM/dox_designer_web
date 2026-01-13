54f6ef95c99f349a6614d6581a95c7124ae6943b
JanishkaM <janishkawebdesign@gmail.com>
2026-01-13 20:36:46 +0530

    fix: update Google Analytics environment variable to VITE_GA_ID and add .env files to .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import { useEffect } from "react";
 
 import ReactGA from 'react-ga4';
 
-const MEASUREMENT_ID = process.env.REACT_APP_GA_ID || "";
+const MEASUREMENT_ID = process.env.VITE_GA_ID || "";
 ReactGA.initialize(MEASUREMENT_ID);
 
 function App() {
